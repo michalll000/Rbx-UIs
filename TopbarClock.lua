ec03228f8c994909cecae389b4f7d429bda2bb73
MixeroTN <40803091+michalll000@users.noreply.github.com>
2020-06-11 13:48:32 +0200

    Update TopbarClock.lua

--- TopbarClock.lua
+++ TopbarClock.lua
@@ -4,7 +4,7 @@ This is a basic OS Clock on Topbar which dissapears when Topbar's HealthBar appe
 StarterGUI
 - Clock - ScreenGUI (Name it how you want, Enabled = true, ResetOnSpawn = true)
 -- Holder - TextLabel (AnchorPoint = Vector2.new(1, 1), BackgroundTransparency = 1, Position = UDim2.new(1, -56, 0, 0), Size = UDim2.new(0, 80, 0, 32), TextTransparency = 1)
---- Time - TextLabel (AnchorPoint = Vector2.new(.5, .5), BackgroundTransparency = 1, Position = UDim2.new(.5, 0, .5, 0), Size = UDim2.new(.95, 0, .75, 0), Font = Enum.Font.GothamBold, TextColor3 = Color3.fromRGB(255, 255, 255), TextScaled = true, TextStrokeTransparency = 1, TextXAlignment = Enum.TextXAlignment.Right)
+--- Time - TextLabel (AnchorPoint = Vector2.new(.5, .5), BackgroundTransparency = 1, Position = UDim2.new(.5, 0, .5, 0), Size = UDim2.new(.95, 0, .75, 0), Font = Enum.Font.GothamBold, TextColor3 = Color3.fromRGB(255, 255, 255), TextScaled = true, TextStrokeTransparency = 1, TextTransparency = 1, TextXAlignment = Enum.TextXAlignment.Right)
 ---- TimeOperator - LocalScript [CURRENT SCRIPT] (Name it how you want)
 ]]
 
